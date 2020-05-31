79517fd29687fe144ba54ae11ffdb01db0856f85
dgjustice <djustice@wificidr.net>
2020-05-31 19:23:26 +0000

    Nikola auto commit.
    Nikola version: 8.0.1

--- git.sh
+++ git.sh
@@ -3,4 +3,4 @@ set -eux
 git config --global url."https://".insteadOf git://
 git config --global user.name "dgjustice"
 git config --global user.email "djustice@wificidr.net"
-git config --global credential.helper "/bin/bash credentials.sh"
+git config --global credential.helper "/bin/bash /app/credentials.sh"
